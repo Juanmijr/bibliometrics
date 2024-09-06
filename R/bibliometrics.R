682c5bfa7c745d1c46db316217bbaf6621fedb17
juanmijr <jmjr0007@red.ujaen.es>
2024-09-06 11:35:17 +0200

    credenciales

--- R/bibliometrics.R
+++ R/bibliometrics.R
@@ -10,22 +10,4 @@
   } else {
     message("TinyTeX ya está instalado.")
   }
-
-    if (!("USER" %in% names(Sys.getenv())) || !("PASSWORD" %in% names(Sys.getenv()))) {
-      user <- readline(prompt = "Introduce tu nombre de usuario: ")
-      password <- readline(prompt = "Introduce tu contraseña: ")
-
-      renviron_path <- file.path(Sys.getenv("HOME"), ".Renviron")
-
-      if (!file.exists(renviron_path)) {
-        file.create(renviron_path)
-      }
-
-      cat(paste0("USER=", user, "\n"), file = renviron_path, append = TRUE)
-      cat(paste0("PASSWORD=", password, "\n"), file = renviron_path, append = TRUE)
-
-      message("Credenciales guardadas en .Renviron. Por favor, reinicia R para aplicar los cambios.")
-    }
-
-
 }
